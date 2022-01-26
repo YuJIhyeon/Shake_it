7ade4879d7ce7d3f98b2ee9df7b05ac14baf7111
YuJihyeon <starsand96@naver.com>
2022-01-26 16:28:50 +0900

    BallTracker Update
    Ball Tracker

---
+++ Assets/Scripts/BallTracer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OpenCvSharp;
+
+public class BallTracer : MonoBehaviour
+{
+    WebCamTexture _webCamTexture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        _webCamTexture = new WebCamTexture(devices[2].name);
+        _webCamTexture.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Renderer>().material.mainTexture = _webCamTexture;
+    }
+}
